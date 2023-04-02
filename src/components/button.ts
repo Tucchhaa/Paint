7b8ed68e7cd9c9c4100374fa09524f7ced4352d5
Eldar Iusupzhanov <eldar.iusupzhanov@devexpress.com>
2023-04-02 23:54:04 +0800

    make a mistake to check CI

--- src/components/button.ts
+++ src/components/button.ts
@@ -1,4 +1,4 @@
-import { Component } from "./core/component";
+import { Component } from "../core/component";
 import { Model, View, Controller } from "./core/module"
 
 class ButtonModel extends Model {
