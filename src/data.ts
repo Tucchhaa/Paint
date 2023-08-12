bd4aed1be4677b6a9cb94916b2e61210d909ce46
Eldar Iusupzhanov <84278206+Tucchhaa@users.noreply.github.com>
2023-08-12 13:50:21 +0600

    implement list example (#19)

---
+++ src/data.ts
@@ -0,0 +1,15 @@
+export const data = [
+    { id: 0, text: 'Организовать созвон в ДСе' },
+    { id: 1, text: 'Закончить туду лист' },
+    { id: 2, text: 'Написать стили' },
+    { id: 3, text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation' },
+    { id: 4, text: 'ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
+    { id: 5, text: 'Duis aute irure dolor in reprehenderit in' },
+    { id: 6, text: 'voluptate velit esse cillum dolore eu fugiat nulla pariatur.' },
+    { id: 7, text: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+    { id: 8, text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation' },
+    { id: 9, text: 'ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
+    { id: 10, text: 'Duis aute irure dolor in reprehenderit in' },
+    { id: 11, text: 'voluptate velit esse cillum dolore eu fugiat nulla pariatur.' },
+    { id: 12, text: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' }
+];
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
-import { Button } from "./components";
+import { Button, Input, List } from "./components";
+import { data } from "./data";
 
 window.addEventListener('load', () => {
-    const buttonContainer = document.getElementById('button');
+    const list = new List<any>(
+        document.getElementById('list'),
+        { selectionEnabled: true },
+        data
+    );
 
-    new Button(buttonContainer, {
-        text: 'aboba',
-        onClick: () => alert('ABOBA!'),
-        styleMode: 'outline',
+    // ===
+
+    const input = new Input(document.getElementById('input'));
+
+    // ===
+
+    new Button(document.getElementById('add-btn'), {
+        text: 'add',
+        onClick: () => createTask(),
     });
+
+    function createTask() {
+        if(input.model.value.length) {
+            list.dataSource.addItem({ text: input.model.value });
+            input.model.value = "";
+            list.container.scrollTo({
+                top: list.container.scrollHeight,
+                behavior: 'smooth'
+            });
+        }
+    }
+    (window as any).input = input;
 });
+
