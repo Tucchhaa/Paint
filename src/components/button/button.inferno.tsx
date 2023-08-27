c59a0ca5d9100790bbb8aa5ffa95a8ef090ff8a0
Eldar Iusupzhanov <ferrarijed@gmail.com>
2023-08-27 17:15:03 +0600

    use fontawesome icons

--- src/components/button/button.inferno.tsx
+++ src/components/button/button.inferno.tsx
@@ -1,4 +1,4 @@
-import { InfernoComponent } from 'core/views/inferno';
+import { InfernoComponent, InfernoProps } from 'core/views/inferno';
 import { ButtonModel, ButtonStyleMode } from './button.model';
 import { ButtonController } from './button.controller';
 import { parseStyleSize } from 'utils/helpers';
@@ -8,9 +8,16 @@ const compileClassName = (styleMode: ButtonStyleMode) => {
 };
 
 export class ButtonInfernoView extends InfernoComponent<ButtonModel> {
+    buttonController: ButtonController;
+
+    constructor(props: InfernoProps<ButtonModel>) {
+        super(props);
+
+        this.buttonController = this.component.getController(ButtonController);
+    }
+
     render() {
-        const { component, model } = this.props;
-        const controller = component.getController(ButtonController) as ButtonController;
+        const { model } = this.props;
 
         const width = parseStyleSize(model.width);
         const height = parseStyleSize(model.height);
@@ -23,7 +30,7 @@ export class ButtonInfernoView extends InfernoComponent<ButtonModel> {
                 class={ className }
                 title={ model.title }
                 style={{ height, width }}
-                onClick={ controller.onClick.bind(controller) }
+                onClick={ (event) => this.buttonController.onClick(event) }
             >
                 <div class='jet-button-content'>
                     <span class='jet-button-text'>{ model.text }</span>
